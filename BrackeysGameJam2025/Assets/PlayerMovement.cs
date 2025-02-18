66e833c985a04d6015cecec78139c86b11a2402a
LouisPerry4281 <louisperry4281@gmail.com>
2025-02-18 10:11:03 +0000

    Player Roll

--- BrackeysGameJam2025/Assets/PlayerMovement.cs
+++ BrackeysGameJam2025/Assets/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
 
@@ -6,20 +8,55 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float rollPower;
+
+    bool isRolling = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isRolling)
+        {
+            StartCoroutine(Roll());
+        }
+    }
+
     void FixedUpdate()
     {
-        MovePlayer();
+        if (!isRolling)
+        {
+            MovePlayer();
+        }
     }
 
     private void MovePlayer()
     {
         rb.linearVelocity = movementSpeed * GetInputDirection();
+    }
+
+    private IEnumerator Roll()
+    {
+        isRolling = true;
+
+        Vector2 initialDir = GetInputDirection();
+
+        if (initialDir == Vector2.zero)
+        {
+            initialDir = Vector2.right;
+        }
+
+        rb.linearVelocity = Vector2.zero;
+        rb.linearVelocity = initialDir * rollPower;
+
+        yield return new WaitForSeconds(0.5f);
+
+        isRolling = false;
+
+        yield return null;
     }
 
     private Vector2 GetInputDirection()
@@ -34,3 +71,5 @@ public class PlayerMovement : MonoBehaviour
         return inputDir;
     }
 }
+
+
